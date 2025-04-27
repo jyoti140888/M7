df9c575629c79df6698660a1fd3131feae5fb66f
jyoti140888 <140839074+jyoti140888@users.noreply.github.com>
2025-04-27 09:02:50 +0530

    Update Sample.java

--- src/test/java/basic/Sample.java
+++ src/test/java/basic/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) 
 	{
-            System.out.println("Welcome to");
+            System.out.println("Welcome to Qspider Jspider Pyspider");
 	}
 
 }
